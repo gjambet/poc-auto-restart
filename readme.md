72670b1c2670d076ea3b1ec3073092a463a0f60d
guillaume <guillaume.jambet@gmail.com>
2022-10-03 22:58:20 +0200

    feat : first minimal working version

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Auto restart poc
+
+minimum code to demo auto restart within the application
+
+Start the application : 
+
+mvn verify spring-boot:run
+
+Test : 
+
+curl -X POST localhost:8080/restart
---
+++ src/main/java/net/guillaume/RestartController.java
@@ -0,0 +1,13 @@
+package net.guillaume;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestartController {
+
+    @PostMapping("/restart")
+    public void restart() {
+        Main.restart();
+    }
+}
